42643f55dcfe16be368f76fe0e2ffac6feee4c4a
Shunyi Chen <shunyichen@Shunyis-MacBook-Pro.local>
2025-12-04 12:16:36 -0500

    hausdsjsabdsqd

---
+++ lab13.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class lab13 {
+    ArrayList<Integer> data = new ArrayList<>();
+    public void readData(File file) {
+        try {
+            FileReader fr = new FileReader(file.getName());
+            BufferedReader br = new BufferedReader(fr);
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                data.add(Integer.parseInt(line));
+            }
+            br.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();;
+        }
+    }
+
+    public long getTotalCount() {
+        return data.stream().count();
+    }
+
+    public long getOddCount() {
+        return data.stream().filter(n -> n % 2 != 0).count();
+    }
+
+    public long getEvenCount() {
+        return data.stream().filter(n -> n % 2 == 0).count();
+    }
+
+    public long getDistinctGreaterThanFiveCount() {
+        return data.stream().filter(n -> n > 5).distinct().count();
+    }
+
+    public Integer[] getResult1() {
+        return data.stream()
+            .filter(n -> n > 5 && n < 50)
+            .filter(n -> n % 2 == 0)
+            .sorted()
+            .toArray(Integer[]::new);
+    }
+
+    public Integer[] getResult2() {
+        return data.stream()
+            .map(n -> 3 * (n * n))
+            .limit(50)
+            .toArray(Integer[]::new);
+    }
+
+    public Integer[] getResult3() {
+        return data.stream()
+            .filter(n -> n% 2 != 0)
+            .map(n -> n * 2)
+            .sorted()
+            .skip(20)
+            .distinct()
+            .toArray(Integer[]::new);
+    }
+}
---
+++ lab13Test.java
@@ -0,0 +1,126 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class lab13Test 
+{
+	lab13 lab;
+	Integer[] expectedResult1;
+	Integer[] expectedResult2;
+	Integer[] expectedResult3;
+	String inputFilename = "lab13_input_data.txt";
+	String expectedResultsFilename = "lab13_expected_results.txt";
+
+	@BeforeEach
+	public void setUp() throws Exception 
+	{
+		// instantiate an instance of lab13
+		lab = new lab13();
+		
+		// read the input values and populate the ArrayList<Integer> in the lab13 class
+		lab.readData(inputFilename);
+		
+		// testData.get(0) has the expected results for getResult1()
+		// testData.get(1) has the expected results for getResult2()
+		// testData.get(2) has the expected results for getResult3()
+		ArrayList<ArrayList<Integer>> testData = new ArrayList<>();
+		for( int i = 0; i < 3; i++ )
+		{
+			testData.add(new ArrayList<Integer>());
+		}
+		
+		try
+		{
+			System.out.println("reading " + expectedResultsFilename);
+			java.io.BufferedReader input = new java.io.BufferedReader(new java.io.InputStreamReader(new java.io.FileInputStream(expectedResultsFilename)));
+			for( int i = 0; i < testData.size(); i++ )
+			{
+				ArrayList<Integer> currentTestData = testData.get(i); 
+				String inn;
+				input.readLine();
+				while( (inn = input.readLine()) != null )
+				{
+					if( inn.length() > 0 )
+					{
+						currentTestData.add(Integer.valueOf(inn));
+					}
+					else
+					{
+						break;
+					}
+				}
+				testData.set(i, currentTestData);
+				System.out.println("testData.get(" + i + ").size() = " + testData.get(i).size());
+			}
+			input.close();
+			
+			// put code here to populate expectedResult1 with the values in testData.get(0)
+			// so that you can use "assertArrayEquals" to verify expectedResult1 and lab.getResult1() are the same
+			expectedResult1 = testData.get(0).toArray(new Integer[0]);
+			
+			
+			// put code here to populate expectedResult2 with the values in testData.get(1)
+			// so that you can use "assertArrayEquals" to verify expectedResult2 and lab.getResult2() are the same
+			expectedResult2 = testData.get(1).toArray(new Integer[0]);
+			
+			
+			// put code here to populate expectedResult3 with the values in testData.get(2)
+			// so that you can use "assertArrayEquals" to verify expectedResult3 and lab.getResult3() are the same
+			expectedResult3 = testData.get(2).toArray(new Integer[0]);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.toString());
+			System.exit(0);
+		}		
+	}
+
+	@Test
+	public void testGetTotalCount() 
+	{
+		assertEquals(1000, lab.getTotalCount());	
+	}
+
+	@Test
+	public void testGetOddCount()
+	{
+		assertEquals(507, lab.getOddCount());	
+	}
+
+	@Test
+	public void testGetEvenCount()
+	{
+		assertEquals(493, lab.getEvenCount());
+	}
+
+	@Test
+	public void testGetDistinctGreaterThanFiveCount()
+	{
+		assertEquals(94, lab.getDistinctGreaterThanFiveCount());
+	}
+
+	@Test
+	public void testGetResult1()
+	{
+		assertArrayEquals(expectedResult1, lab.getResult1());
+	}
+
+	@Test
+	public void testGetResult2()
+	{
+		assertArrayEquals(expectedResult2, lab.getResult2());
+	}
+
+	@Test
+	public void testGetResult3()
+	{
+		assertArrayEquals(expectedResult3, lab.getResult3());
+	}
+
+}
